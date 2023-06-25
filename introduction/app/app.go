004e4689aefa3f26fea8fa6b974993aa7de744af
Praveen Hosamani <praveen.h397@gmail.com>
2023-06-25 17:25:14 +0200

    Global and Local varibales

--- introduction/app/app.go
+++ introduction/app/app.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
+// declaring the global varibales
+var greetings = "Hello,"
+
 func main() {
     // fmt.Println("hello world")
     customer := getData(1)
-    fmt.Println(customer)
+    fmt.Println(greetings + customer)
 }
 
 // func getData(variable dataType_var)(outputs){}
@@ -15,6 +18,6 @@ func getData(customerId int)(customer string){
     var firstName = "Praveen"
     lastName := "Hosamani" //:= initializing purpose
     // var fullName string
-    
+
     return firstName + " " + lastName
 }
