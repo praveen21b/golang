3cfb5c9ef678ed8857a4595b04aef15525ee1d38
Praveen Hosamani <praveen.h397@gmail.com>
2023-06-25 18:15:36 +0200

    array datatype introduced

--- introduction/app/app.go
+++ introduction/app/app.go
@@ -7,24 +7,17 @@ var greetings = "Hello,"
 
 func main() {
     // fmt.Println("hello world")
-    customer := getData(2)
-    fmt.Println(greetings + customer)
+    customers := getData()
+    fmt.Println(customers)
 }
 
 // func getData(variable dataType_var)(outputs){}
-func getData(customerId int)(customer string){
+func getData()(customer [4]string){
+    // array
+    customer[0] = "Praveen"
+    customer[1] = "Sharada"
+    customer[2] = "Pragati"
+    customer[3] = "Prashant"
 
-    if customerId == 1{
-        // traditional way of initializing 
-    var firstName = "Praveen"
-    lastName := "Hosamani" //:= initializing purpose
-    // var fullName string
-
-    return firstName + " " + lastName
-    } else if customerId == 2 {
-        return "Sharada Hosamani"
-    } else {
-        return " No customer name!!"
-    }
-    
+    return customer
 }
