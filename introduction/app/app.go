56e6538e7a496e570c9370dcc2aec778ba10175b
Praveen Hosamani <praveen.h397@gmail.com>
2023-06-25 17:09:45 +0200

    Added a function to app.go

--- introduction/app/app.go
+++ introduction/app/app.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+    // fmt.Println("hello world")
+    customer := getData(1)
+    fmt.Println(customer)
+}
+
+// func getData(variable dataType_var)(outputs){}
+func getData(customerId int)(customer string){
+
+    // traditional way of initializing 
+    var firstName = "Praveen"
+    lastName := "Hosamani" //:= initializing purpose
+    // var fullName string
+    
+    return firstName + " " + lastName
 }
